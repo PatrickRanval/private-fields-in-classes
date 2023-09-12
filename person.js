670a0a90f0d8ba5c0877b7e1989c5bbf7747bb6c
PatrickRanval <pat.ranval@gmail.com>
2023-09-12 16:10:23 -0500

    Passing Class Definition for "private fields in classes"
    Defined a Person class with a private ID property that iterates upon invoking the class constructor.

--- person.js
+++ person.js
@@ -1,5 +1,20 @@
 class Person {
-  // Write code here
+     static #id = 0;
+     constructor () {
+          this._ID = Person.#id++;          
+     };
+
+  getID() {
+     return this._ID;
+  }
+
 }
 
 module.exports = Person;
+
+// const guy = new Person();
+// const bro = new Person();
+// console.log(guy.getID());
+// console.log(bro.getID());
+//console.log(bro.#id);
+//Above demonstates private property cannot be accessed from within instance of class object
